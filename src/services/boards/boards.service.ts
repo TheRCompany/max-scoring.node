9e75389fb4afb63ebf18018d7d9d5ed12e88c2e8
whispyy <jfdurk@gmx.fr>
2019-09-17 19:36:54 -0400

    core(*): init feathers with ts

---
+++ src/services/boards/boards.service.ts
@@ -0,0 +1,31 @@
+// Initializes the `boards` service on path `/boards`
+import { ServiceAddons } from '@feathersjs/feathers';
+import { Application } from '../../declarations';
+import { Boards } from './boards.class';
+import createModel from '../../models/boards.model';
+import hooks from './boards.hooks';
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    'boards': Boards & ServiceAddons<any>;
+  }
+}
+
+export default function (app: Application) {
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/boards', new Boards(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('boards');
+
+  service.hooks(hooks);
+}
---
+++ test/services/boards.test.ts
@@ -0,0 +1,8 @@
+import app from '../../src/app';
+
+describe('\'boards\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('boards');
+    expect(service).toBeTruthy();
+  });
+});
