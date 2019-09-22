57ea0a43bfd5a16a2f116fd90b74483fc6526a24
Maxime Damour <maxime.damour974@gmail.com>
2019-09-21 21:29:19 -0400

    test push master

--- src/services/boards/boards.class.ts
+++ src/services/boards/boards.class.ts
@@ -1,6 +1,6 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '../../declarations';
-
+// allo
 export class Boards extends Service {
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
